9c3b30d182ef48d361ecd2d66ea32c82eaf0eeb4
Lazaro Reina <debian.gnu.lazaro@gmail.com>
2020-07-24 04:36:01 -0300

    24/07/2020

--- opcao.py
+++ opcao.py
@@ -3,3 +3,5 @@ from paridadeputcall import Opcao
 ticker = 'BBDC4'
 opt = Opcao(ticker)
 
+opt.get_values()
+print(opt.ispair())
--- paridadeputcall.py
+++ paridadeputcall.py
@@ -5,8 +5,22 @@ class Opcao:
     def __init__(self, ticker):
         self.ticker = ticker
 
-    values = {'c':0.0, 'K':0.0, 'r':0.0, 'p':0.0, 'So':0.0 }
+    values = {'c':0.0, 'K':0.0, 'r':0.0, 'T':0.0, 'p':0.0, 'So':0.0 }
 
-    
+    def privateimput(self, key):
+        return float(input('Digite a variável {}: '.format(key)))
 
+    def get_values(self):
+        for c in self.values:
+            self.values[c] = self.privateimput(c)
+
+    def ispair(self):
+        portfolio_a = self.values['c'] + self.values['K'] * math.exp(self.values['r'] * self.values['T'] * - 1)
+        portfolio_b = self.values['p'] + self.values['So']
+        print('{:.2f}'.format(portfolio_a), '\n', '{:.2f}'.format(portfolio_b))
+
+        if portfolio_a != portfolio_b:
+            return False
+
+        return True
     
